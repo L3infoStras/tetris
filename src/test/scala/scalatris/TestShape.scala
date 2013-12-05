490340a22e60071005fc5da198e0cc61be2bb5c4
Clément Schreiner <clement@mux.me>
2013-12-05 15:03:27 +0100

    Add test for shape (draft - useless).

---
+++ src/test/scala/scalatris/TestShape.scala
@@ -0,0 +1,27 @@
+package scalatris
+
+import org.scalatest._
+import prop._
+import scala.collection.immutable._
+
+class SetSpec extends PropSpec with TableDrivenPropertyChecks with Matchers {
+  val examples =
+    Table(
+      "set",
+      BitSet.empty,
+      HashSet.empty[Int],
+      TreeSet.empty[Int]
+    )
+
+  property("an empty Set should have size 0") {
+    forAll(examples) { set =>
+      set.size should be (0)
+    }
+  }
+
+  property("invoking head on an empty set should produce NoSuchElementException") {
+    forAll(examples) { set =>
+       a [NoSuchElementException] should be thrownBy { set.head }
+    }
+  }
+}
