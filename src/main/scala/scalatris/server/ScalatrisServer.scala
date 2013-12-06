de3b93b87c099c738cf2e9ec826fc056b49df436
Julien Gamba <jgamba@aius.u-strasbg.fr>
2013-12-06 01:08:04 +0100

    Dummy server

--- src/main/scala/scalatris/server/ScalatrisServer.scala
+++ src/main/scala/scalatris/server/ScalatrisServer.scala
@@ -1,8 +1,22 @@
 package scalatris.server
 
+import java.io._
+import java.net.{InetAddress,ServerSocket,Socket,SocketException}
+
 object ScalatrisServer {
 
-  def main(args: Array[String]) {
-    println("Scalatris server starting...")
-  }
+  def main(args: Array[String]): Unit = {
+      try {
+        val listener = new ServerSocket(9999);
+        println("Scalatris server starting...")
+        while (true)
+          listener.accept();
+        listener.close()
+      }
+      catch {
+        case e: IOException =>
+          System.err.println("Could not listen on port: 9999.");
+          System.exit(-1)
+      }
+    }
 }
