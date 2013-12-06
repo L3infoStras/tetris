20f5441dc158b3daafd16bc6eabc00623a302d04
Julien Gamba <jgamba@aius.u-strasbg.fr>
2013-12-06 01:21:09 +0100

    Minimalistic scala server

--- src/main/scala/scalatris/server/ScalatrisServer.scala
+++ src/main/scala/scalatris/server/ScalatrisServer.scala
@@ -1,22 +1,22 @@
 package scalatris.server
 
 import java.io._
-import java.net.{InetAddress,ServerSocket,Socket,SocketException}
+import java.net._
+import scala.io._
 
 object ScalatrisServer {
 
-  def main(args: Array[String]): Unit = {
-      try {
-        val listener = new ServerSocket(9999);
-        println("Scalatris server starting...")
-        while (true)
-          listener.accept();
-        listener.close()
-      }
-      catch {
-        case e: IOException =>
-          System.err.println("Could not listen on port: 9999.");
-          System.exit(-1)
-      }
+  def main(args: Array[String]) {
+    val server = new ServerSocket(9999);
+    println("Scalatris server starting...")
+    while (true) {
+      val s = server.accept()
+      val in = new BufferedSource(s.getInputStream()).getLines()
+      val out = new PrintStream(s.getOutputStream())
+
+      out.println(in.next())
+      out.flush()
+      s.close()
     }
+  }
 }
