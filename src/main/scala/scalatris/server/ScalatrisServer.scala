284684b34a6bd7596a4326268e90a697e992ccf9
Clément Schreiner <clement@mux.me>
2013-12-07 16:19:48 +0100

    First real draft of the GUI.

--- src/main/scala/scalatris/server/ScalatrisServer.scala
+++ src/main/scala/scalatris/server/ScalatrisServer.scala
@@ -3,8 +3,10 @@ package scalatris.server
 import scalatris._
 
 import swing._
-
+import event.Key._
 import scala.swing.event._
+
+//import scala.swing.event._
 import java.awt.{BasicStroke}
 import java.awt.{Color => AWTColor}
 
@@ -23,6 +25,26 @@ class GridCanvas(val grid: TetrisGrid) extends Component {
     val x0 = 0
     val y0 = 0
 
+    def fillCell(x:Int, y:Int) {
+      g.setColor(lightBluishGray)
+      g.fillRect(x0 + x*cellSize, y0 + y*cellSize, cellSize, cellSize)
+    }
+
+    def drawVerticalLines {
+      g.setColor(bluishSilver)
+      for (x <- 0 to nbCols)
+        g.draw(new Line2D.Double(x0 + x * cellSize, y0, x0 + x * cellSize, y0 + cellSize*nbRows))
+    }
+
+    def drawHorizontalLines {
+      g.setColor(bluishSilver)
+      for (y <- 0 to nbRows)
+        g.draw(new Line2D.Double(x0, y0 + y * cellSize, x0 + cellSize*nbCols, y0 + y * cellSize))
+    }
+
+
+
+
     g.setRenderingHint(java.awt.RenderingHints.KEY_ANTIALIASING, 
 		       java.awt.RenderingHints.VALUE_ANTIALIAS_ON)
     g.setColor(lightBluishGray)
@@ -30,14 +52,18 @@ class GridCanvas(val grid: TetrisGrid) extends Component {
     g.setColor(bluishGray)
     g.fillRect(0, 0, nbCols*cellSize, nbRows*cellSize)
 
-    g.setColor(bluishSilver)
-    // vertical lines
-    for (x <- 0 to nbCols)
-      g.draw(new Line2D.Double(x0 + x * cellSize, y0, x0 + x * cellSize, y0 + cellSize*nbRows))
-    // horizontal lines
-    for (y <- 0 to nbRows)
-      g.draw(new Line2D.Double(x0, y0 + y * cellSize, x0 + cellSize*nbCols, y0 + y * cellSize))
+    fillCell(4, 5)
+    fillCell(4, 6)
+    fillCell(4, 7)
+    fillCell(5, 7)
+
+
+
+    drawVerticalLines
+    drawHorizontalLines
+
     g.setStroke(new BasicStroke(3f))
+
 
   }
 }
@@ -48,13 +74,43 @@ object ScalatrisGui extends SimpleSwingApplication {
 
   val canvas = new GridCanvas(grid)
 
+  val label =  new Label {
+      text = "No click yet"
+  }
+
   def top = new MainFrame {
     title = "Scalatris"
     contents = mainPanel
     }
 
+  def onKeyPress(keyCode: Value) = keyCode match {
+    case Left | H => label.text = "LEFT"
+    case Right | L => label.text = "RIGHT"
+    case Down | J => label.text = "DOWN"
+    case Space => label.text = "SPACE"
+    case Escape | Q => quit
+    case _ => println("Unknown key " ++ keyCode.toString)
+  }
+
+
   def mainPanel = new BoxPanel(Orientation.Horizontal) {
+
+
     preferredSize = new Dimension(640, 480)
     contents += canvas
+    contents += label
+
+    focusable = true
+    requestFocus
+
+    listenTo(keys)
+    reactions += {
+      case KeyPressed(_, key, _, _) =>
+        onKeyPress(key)
+        repaint
+    }
+
+
   }
+
 }
