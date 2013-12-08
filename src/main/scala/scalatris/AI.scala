3528c77b883a45c8c3544695c21987d74d5cdc53
J4g0n <j4g0n@Shakespeare.(none)>
2013-12-08 14:32:18 +0100

    ia finie mais non débuggée :)

--- src/main/scala/scalatris/AI.scala
+++ src/main/scala/scalatris/AI.scala
@@ -7,12 +7,19 @@ class AI (tg:TetrisGrid) {
   val left = 'q'
   val right = 'd'
   val bot = 's'
-  def listGrid: List[(AIgrid, List[Char])] = {
+  val rot = 'z'
+
+  // bon le code c'est un peu de la merde mais il faudrait rajouter des constructeurs dans AIgrid pour 
+  // clarifier. Mais l'algo devrait marcher.
+  def listGrid (ag:AIgrid) (preInput:List[Char]): List[(AIgrid, List[Char])] = {
     def reachBot (aig:AIgrid, mvl:List[Char]): (AIgrid,List[Char]) = {
       if(aig.possibleMvDown) 
         reachBot (new AIgrid(aig.shapeAbs-1,aig.shapeOrd,aig.curShape,aig.grid),
                  mvl :+ bot)
-      else (aig,mvl)
+      else {
+        aig.draw
+        (aig,preInput++mvl)
+      }
     }
     def reachLeft (aig:AIgrid, mvl:List[Char]): List[(AIgrid,List[Char])] = {
       if(aig.possibleMvLeft)
@@ -26,8 +33,25 @@ class AI (tg:TetrisGrid) {
                    right::mvl) :+ reachBot (aig, mvl)
       else Nil
     }
-    val feedLeft = new AIgrid (tg.shapeAbs, tg.shapeOrd-1, tg.curShape, tg.grid)
-    val feedRight = new AIgrid (tg.shapeAbs, tg.shapeOrd, tg.curShape, tg.grid)
-    reachLeft (feedLeft,Nil) ++ reachRight (feedRight,Nil)
+    reachLeft (new AIgrid (ag.shapeAbs, ag.shapeOrd-1, ag.curShape, ag.grid),List(left)) ++ reachRight (ag,Nil)
+  }
+
+  def listWithRot (ag:AIgrid): List[(AIgrid, List[Char])] = {
+    def listRot (aig:AIgrid) (shStop:Shape) (preInput:List[Char]): List[(AIgrid, List[Char])] = {
+      if(aig.curShape!=shStop) 
+        listGrid (new AIgrid(aig.shapeAbs-1, aig.shapeOrd, aig.curShape.rotation, aig.grid)) (preInput :+ rot)
+      else Nil
+    }
+    listGrid (ag) (Nil) ++ 
+    listRot (new AIgrid(ag.shapeAbs-1, ag.shapeOrd, ag.curShape.rotation, ag.grid)) (ag.curShape) (List(bot,rot))
+  }
+
+  def computeAI: List[Char] = {
+    def choseVal (v1:(Double,List[Char]),v2:(Double,List[Char])): (Double,List[Char]) = {
+      if (v1._1>v2._1) v1
+      else v2
+    }
+    val gridList = listWithRot (new AIgrid(tg.shapeAbs-1, tg.shapeOrd, tg.curShape.rotation, tg.grid)) 
+    ((gridList map (v => (v._1.eval,v._2))) reduceLeft (choseVal (_,_)))._2
   }
 }
