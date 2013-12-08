5649a5ceb03d25e06afbd54ea4181faf7d3b4ec9
J4g0n <j4g0n@Shakespeare.(none)>
2013-12-08 18:37:04 +0100

    ia presque fonctionnelle

--- src/main/scala/scalatris/AI.scala
+++ src/main/scala/scalatris/AI.scala
@@ -4,46 +4,51 @@ import Array._
 import scala.util._
 
 class AI (tg:TetrisGrid) {
-  val left = 'h'
-  val right = 'j'
-  val bot = 'k'
-  val rot = 'l'
+  val left = 'q'
+  val right = 'd'
+  val bot = 's'
+  val rot = 'z'
 
   // bon le code c'est un peu de la merde mais il faudrait rajouter des constructeurs dans AIgrid pour 
   // clarifier. Mais l'algo devrait marcher.
   def listGrid (ag:AIgrid) (preInput:List[Char]): List[(AIgrid, List[Char])] = {
     def reachBot (aig:AIgrid, mvl:List[Char]): (AIgrid,List[Char]) = {
       if(aig.possibleMvDown) 
-        reachBot (new AIgrid(aig.shapeAbs-1,aig.shapeOrd,aig.curShape,aig.grid),
+        reachBot (new AIgrid(aig.shapeAbs+1,aig.shapeOrd,aig.curShape,aig.grid.clone),
                  mvl :+ bot)
       else {
         aig.draw
+        aig.printGrid
+        println(preInput++mvl)
         (aig,preInput++mvl)
       }
     }
     def reachLeft (aig:AIgrid, mvl:List[Char]): List[(AIgrid,List[Char])] = {
       if(aig.possibleMvLeft)
-        reachLeft (new AIgrid(aig.shapeAbs,aig.shapeOrd-1,aig.curShape,aig.grid),
+        reachLeft (new AIgrid(aig.shapeAbs,aig.shapeOrd-1,aig.curShape,aig.grid.clone),
                   left::mvl) :+ reachBot (aig, mvl)
       else Nil
     }
     def reachRight (aig:AIgrid, mvl:List[Char]): List[(AIgrid,List[Char])] = {
-      if(aig.possibleMvLeft)
-        reachRight (new AIgrid(aig.shapeAbs,aig.shapeOrd+1,aig.curShape,aig.grid),
+      if(aig.possibleMvRight)
+        reachRight (new AIgrid(aig.shapeAbs,aig.shapeOrd+1,aig.curShape,aig.grid.clone),
                    right::mvl) :+ reachBot (aig, mvl)
       else Nil
     }
-    reachLeft (new AIgrid (ag.shapeAbs, ag.shapeOrd-1, ag.curShape, ag.grid),List(left)) ++ reachRight (ag,Nil)
+    reachLeft (new AIgrid (ag.shapeAbs, ag.shapeOrd-1, ag.curShape, ag.grid.clone),List(left)) ++ reachRight (ag,Nil)
   }
 
   def listWithRot (ag:AIgrid): List[(AIgrid, List[Char])] = {
     def listRot (aig:AIgrid) (shStop:Shape) (preInput:List[Char]): List[(AIgrid, List[Char])] = {
-      if(aig.curShape!=shStop) 
-        listGrid (new AIgrid(aig.shapeAbs-1, aig.shapeOrd, aig.curShape.rotation, aig.grid)) (preInput :+ rot)
+      if(aig.curShape!=shStop) {
+        val naig = new AIgrid(aig.shapeAbs, aig.shapeOrd, aig.curShape.rotation, aig.grid.clone)
+        listGrid (aig) (preInput) ++ 
+        listRot (naig) (shStop) (preInput :+ rot)
+      }
       else Nil
     }
     listGrid (ag) (Nil) ++ 
-    listRot (new AIgrid(ag.shapeAbs-1, ag.shapeOrd, ag.curShape.rotation, ag.grid)) (ag.curShape) (List(bot,rot))
+    listRot (new AIgrid(ag.shapeAbs+1, ag.shapeOrd, ag.curShape.rotation, ag.grid.clone)) (ag.curShape) (List(bot,rot))
   }
 
   def computeAI: List[Char] = {
@@ -51,7 +56,7 @@ class AI (tg:TetrisGrid) {
       if (v1._1>v2._1) v1
       else v2
     }
-    val gridList = listWithRot (new AIgrid(tg.shapeAbs-1, tg.shapeOrd, tg.curShape.rotation, tg.grid)) 
+    val gridList = listWithRot (new AIgrid(tg.shapeAbs+1, tg.shapeOrd, tg.curShape.rotation, tg.grid.clone)) 
     ((gridList map (v => (v._1.eval,v._2))) reduceLeft (choseVal (_,_)))._2
   }
 }
--- src/main/scala/scalatris/Main.scala
+++ src/main/scala/scalatris/Main.scala
@@ -11,6 +11,8 @@ object Main {
       for (i <- 0 until 2) yield g.setCase(16,i) (true)
       var input: Char = Console.readChar
       val aig = new AIgrid(4,5,g.curShape,g.grid)
+      val ai = new AI(aig)
+      println(ai.computeAI)
       g.printGrid
       println(aig.eval)
       while(input!='r')
