1483ef17ee0d947bacc57426184642d83f7b9a17
Julien Gamba <jgamba@aius.u-strasbg.fr>
2013-12-08 20:36:22 +0100

    Added comments

--- src/main/scala/telnetris/TelnetServer.scala
+++ src/main/scala/telnetris/TelnetServer.scala
@@ -6,19 +6,18 @@ import org.jboss.netty.bootstrap.ServerBootstrap
 import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory
 
 /**
- * Simplistic telnet server.
+ * Telnet server for communication between IA and the tetris game
  */
-object TelnetServer {
-
-  def main(args: Array[String]) {
-    // Configure the server.
+object TelnetServer{
+  def main(args: Array[String]){
+    // Server configuration
     val bootstrap = new ServerBootstrap(
       new NioServerSocketChannelFactory(Executors.newCachedThreadPool, Executors.newCachedThreadPool))
 
-    // Configure the pipeline factory.
+    // Pipeline factory configuration
     bootstrap.setPipelineFactory(new TelnetServerPipelineFactory)
 
-    // Bind and start to accept incoming connections.
-    bootstrap.bind(new InetSocketAddress(8080))
+    // Bind to port and start the server
+    bootstrap.bind(new InetSocketAddress(9000))
   }
 }
--- src/main/scala/telnetris/TelnetServerHandler.scala
+++ src/main/scala/telnetris/TelnetServerHandler.scala
@@ -1,7 +1,7 @@
 package telnetris
 
 import java.util.concurrent.atomic.AtomicLong
-import org.jboss.netty.buffer.{ ChannelBuffer, ChannelBuffers }
+import org.jboss.netty.buffer.{ChannelBuffer,ChannelBuffers}
 import org.jboss.netty.channel.{
   Channel,
   ChannelEvent,
@@ -20,20 +20,20 @@ import org.jboss.netty.channel.ChannelFutureListener
 /**
  * Handles a server-side channel.
  */
-class TelnetServerHandler extends SimpleChannelUpstreamHandler {
+class TelnetServerHandler extends SimpleChannelUpstreamHandler{
 
   private val logger = Logger.getLogger(getClass.getName)
 
-  override def handleUpstream(ctx: ChannelHandlerContext, e: ChannelEvent) {
-    e match {
-      case c: ChannelStateEvent ⇒ logger.info(e.toString)
-      case _                    ⇒
+  override def handleUpstream(ctx: ChannelHandlerContext, e: ChannelEvent){
+    e match{
+      case c: ChannelStateEvent => logger.info(e.toString)
+      case _                    =>
     }
     super.handleUpstream(ctx, e)
   }
 
-  override def channelConnected(ctx: ChannelHandlerContext, e: ChannelStateEvent) {
-    // Send greeting for a new connection.
+  override def channelConnected(ctx: ChannelHandlerContext, e: ChannelStateEvent){
+    // Message send to user on connection
     e.getChannel.write(
       "Welcome to " + InetAddress.getLocalHost.getHostName + "!\r\n")
     e.getChannel.write("It is " + new Date + " now.\r\n")
@@ -41,11 +41,11 @@ class TelnetServerHandler extends SimpleChannelUpstreamHandler {
 
   override def messageReceived(ctx: ChannelHandlerContext, e: MessageEvent) {
 
-    // Cast to a String first.
-    // We know it is a String because we put some codec in TelnetPipelineFactory.
+    // First we cast the message to a String
+    // The result will be a String thanks to the codec in TelnetPipelineFactory
     val request = e.getMessage.toString
 
-    // Generate and write a response.
+    // Create a response (will probably change in a close future)
     var response: String = ""
     var close: Boolean = false
     if (request.length == 0) {
@@ -58,19 +58,18 @@ class TelnetServerHandler extends SimpleChannelUpstreamHandler {
       println(request)
     }
 
-    // We do not need to write a ChannelBuffer here.
-    // We know the encoder inserted at TelnetPipelineFactory will do the conversion.
+    // Send the response to the user
+    // The response is first converted by the pipeline
     val future = e.getChannel.write(response)
 
-    // Close the connection after sending 'Have a good day!'
-    // if the client has sent 'bye'.
+    // Close the connection
     if (close) {
       future.addListener(ChannelFutureListener.CLOSE)
     }
   }
 
   override def exceptionCaught(context: ChannelHandlerContext, e: ExceptionEvent) {
-    // Close the connection when an exception is raised.
+    // Log the error then close the connection if an exception is raised
     logger.warning("Unexpected exception from downstream." + e.getCause)
     e.getChannel.close()
   }
